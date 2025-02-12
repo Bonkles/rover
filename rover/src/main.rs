193899765fe09bdfb7ad13b5e2c48b4b4b4628b2
Bonkles <bennyc@gmail.com>
2025-02-12 00:23:45 -0500

    Use clap to parse a couple arguments.

---
+++ rover/src/main.rs
@@ -0,0 +1,12 @@
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli { 
+    pattern: String,
+    path: std::path::PathBuf,
+}
+fn main() {
+    let args = Cli::parse(); 
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+}
