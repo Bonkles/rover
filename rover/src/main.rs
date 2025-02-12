ada6d8f99bfacd1934ad18a1ab736a8e841ad840
Benjamin Clark <clarkben@meta.com>
2025-02-12 16:27:17 -0500

    Read input and match to the supplied pattern arg.

--- rover/src/main.rs
+++ rover/src/main.rs
@@ -8,5 +8,13 @@ struct Cli {
 }
 fn main() {
     let args = Cli::parse(); 
+
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let lines = content.lines();
+    for line in lines {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
     println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
